f67bb6dc55373b6dc9498a94e588ff9c7d0420d6
DeVoresyah ArEst <de.voresyah@gmail.com>
2020-08-23 16:35:04 +0700

    Update rully

---
+++ App/Components/CardProduct.js
@@ -0,0 +1,22 @@
+import React, { memo, useContext } from 'react'
+import { TouchableOpacity,Image, View, Text } from 'react-native'
+import styles from './Styles/CardProduct'
+import { NavigationContext } from "react-navigation"
+import Format from '../Lib/NumberFormat'
+
+const CardProduct = (props) => {
+  const {item, ...restProps} = props
+  const navigation = useContext(NavigationContext)
+
+  return (
+    <TouchableOpacity activeOpacity={0.9} style={styles.card} {...restProps}>
+      <Image source={{ uri: item?.thumbnail }} style={styles.thumb} resizeMode="cover" />
+      <View style={styles.content}>
+        <Text style={styles.title}>{item?.title}</Text>
+        <Text style={styles.price}>{'Rp' + new Format().formatMoney(item?.price ?? 0)}</Text>
+      </View>
+    </TouchableOpacity>
+  )
+}
+
+export default memo(CardProduct)
