4c66e4e5005c35c7546b4fb23b28cea5b9385141
Rizki Budi Prasetyo <rizkibudi513@gmail.com>
2020-09-11 11:03:25 +0700

    Rename Project, App, and Package Name

--- android/app/src/main/java/dev/cloudgakkai/alfredo/MainActivity.java
+++ android/app/src/main/java/dev/cloudgakkai/alfredo/MainActivity.java
@@ -1,4 +1,4 @@
-package com.covidapp;
+package dev.cloudgakkai.alfredo;
 
 import com.facebook.react.ReactActivity;
 
@@ -24,6 +24,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "CovidApp";
+    return "Alfredo";
   }
 }
--- android/app/src/main/java/dev/cloudgakkai/alfredo/MainApplication.java
+++ android/app/src/main/java/dev/cloudgakkai/alfredo/MainApplication.java
@@ -1,4 +1,4 @@
-package com.covidapp;
+package dev.cloudgakkai.alfredo;
 
 import android.app.Application;
 import android.content.Context;
@@ -66,7 +66,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.covidapp.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("dev.cloudgakkai.alfredo.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
